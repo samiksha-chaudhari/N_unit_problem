08f36cffdb58d5ef62c1d18e4c0fa1e5d05b9540
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-09-04 21:39:58 +0530

    vending machine

--- Nunit_testing/Program.cs
+++ Nunit_testing/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+public class VENDING
+{	public static void countCurrency(int amount)
+	{
+		int[] notes = new int[] { 2000, 500, 200, 100, 50, 20, 10, 5, 1 };
+		int[] noteCounter = new int[9];
 
-namespace Nunit_testing
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("N unit testing");
-        }
-    }
+		for (int i = 0; i < 9; i++)
+		{
+			if (amount >= notes[i])
+			{
+				noteCounter[i] = amount / notes[i];
+				amount = amount - noteCounter[i] * notes[i];
+			}
+		}
+
+		// Print notes
+		Console.WriteLine("Currency Count ->");
+		for (int i = 0; i < 9; i++)
+		{
+			if (noteCounter[i] != 0)
+			{
+				Console.WriteLine(notes[i] + " : "
+					+ noteCounter[i]);
+			}
+		}
+	}
+	public static void Main()
+	{
+		Console.WriteLine("Enter amount:");
+		int amount = Convert.ToInt32(Console.ReadLine());
+		countCurrency(amount);
+	}
+
+
 }
