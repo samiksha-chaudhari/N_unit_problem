4124cd464f861e7c6f56257d1d0557c154abbce0
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-09-05 22:07:57 +0530

    payment

--- Nunit_testing/Program.cs
+++ Nunit_testing/Program.cs
@@ -6,7 +6,28 @@ namespace Nunit_testing
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("N unit testing");
+            int p, y, r;
+            double rate, a, b, c, d, pay;
+            Console.WriteLine("Enter the Principal : ");
+            p = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Year : ");
+            y = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Rate : ");
+            r = int.Parse(Console.ReadLine());
+            int n = (12 * y);
+            Console.WriteLine("n : " +n);
+            rate = (r / (12*100));
+            Console.WriteLine("rate :" +rate);
+            a = (p * rate);
+            Console.WriteLine("a : " +a);
+            b = (1 + rate);
+            Console.WriteLine("b : " +b);
+            c = (1 - b);
+            Console.WriteLine("c : " +c);
+            d =Math.Pow(c, (-n));
+            Console.WriteLine("d : "+d);
+            pay = (a / d);
+            Console.WriteLine(pay);
         }
     }
 }
