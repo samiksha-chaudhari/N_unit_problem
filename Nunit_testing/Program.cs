6940c9e4ba2cdd452f5e34d2e372d7a77eb545fb
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-09-06 00:45:45 +0530

    find day

--- Nunit_testing/Program.cs
+++ Nunit_testing/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-
 class Sample
 {
     static void dayofweek(int day,int month, int year)
@@ -12,41 +11,10 @@ class Sample
         h = h % 7;
         string[] days = { "Saturday", "Sunday","Monday","Tuesday","Wednesday","Thursday","Friday" };
         Console.WriteLine("Day for given input is :" +days[h]);
-        /* switch (h)
-        {
-            case 0:
-                Console.WriteLine("Saturday");
-                break;
-
-            case 1:
-                Console.WriteLine("Sunday");
-                break;
-
-            case 2:
-                Console.WriteLine("Monday");
-                break;
-
-            case 3:
-                Console.WriteLine("Tuesday");
-                break;
-
-            case 4:
-                Console.WriteLine("Wednesday");
-                break;
-
-            case 5:
-                Console.WriteLine("Thursday");
-                break;
-
-            case 6:
-                Console.WriteLine("Friday");
-                break;
-        }*/
     }
 
     public static void Main()
     {
-        
         Console.WriteLine("Enter year :");
         int y = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Enter date :");
@@ -54,8 +22,5 @@ class Sample
         Console.WriteLine("Enter month  :");
         int m = Convert.ToInt32(Console.ReadLine());
         dayofweek(d,m,y);
-       // DateTime dt = new DateTime(y, d, m);
-        
-        //Console.WriteLine("The day of the week for {0:d} is {1}.", dt, dt.DayOfWeek);
     }
 }
