bc536d4451c29487197ae203bcabddde78172d3e
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-09-06 15:44:04 +0530

    monthly payment

--- Nunit_testing/Program.cs
+++ Nunit_testing/Program.cs
@@ -4,30 +4,27 @@ namespace Nunit_testing
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            int p, y, r;
-            double rate, a, b, c, d, pay;
+            double p, y, r,n,rate, pay;
             Console.WriteLine("Enter the Principal : ");
-            p = int.Parse(Console.ReadLine());
+            p = int.Parse(Console.ReadLine());  
+
             Console.WriteLine("Enter the Year : ");
             y = int.Parse(Console.ReadLine());
+            
             Console.WriteLine("Enter the Rate : ");
             r = int.Parse(Console.ReadLine());
-            int n = (12 * y);
-            Console.WriteLine("n : " +n);
+            
+            n = (12 * y);
+            //Console.WriteLine("n : " +n);
             rate = (r / (12*100));
-            Console.WriteLine("rate :" +rate);
-            a = (p * rate);
-            Console.WriteLine("a : " +a);
-            b = (1 + rate);
-            Console.WriteLine("b : " +b);
-            c = (1 - b);
-            Console.WriteLine("c : " +c);
-            d =Math.Pow(c, (-n));
-            Console.WriteLine("d : "+d);
-            pay = (a / d);
-            Console.WriteLine(pay);
+            //Console.WriteLine("rate :" +rate);
+
+            pay = (p * rate) / (1 - Math.Abs(Math.Pow((1 + rate), (-n))));
+           
+            Console.WriteLine("payment is :" +pay);
         }
     }
 }
